38b3cf43e187bfbbb7fefcd7bb74497f3d4bffd9
Creative-Md-Arif <mohammadarif.ma02@gmail.com>
2024-08-27 01:49:26 +0600

    first commit

---
+++ app/src/main/java/com/example/assignment2/PlanATripActivity.kt
@@ -0,0 +1,15 @@
+package com.example.assignment2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PlanATripActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_plan_atrip)
+
+    }
+}
